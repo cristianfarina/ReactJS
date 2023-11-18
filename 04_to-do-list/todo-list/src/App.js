91402ebd59c1ba528941f88e500a7e2438ebc60f
cristianfarina <farina.cristtian@gmail.com>
2023-11-18 11:05:30 -0300

    Cambios en App.js de todolist

--- 04_to-do-list/todo-list/src/App.js
+++ 04_to-do-list/todo-list/src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 function App() {
 
   const [tasks, setTasks] = useState([])
+  const [newTasks, setNewTasks] = useState([])
+
 
   const addTask = (newTask) => {
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <TodoList tasks={tasks} />
+        <input type="text" placeholder="Nueva tarea" />
+        <button onClick={() => addTask("Nueva tarea")}>Agregar tarea</button>
+      </div>
+    );  
 
   }
 
